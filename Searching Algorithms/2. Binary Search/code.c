913c64de723100eb56fcfff459a3d5224eb92cf2
ferhad <137129786+ferhadnaiyev@users.noreply.github.com>
2025-03-02 20:58:23 +0400

    binary search

--- Searching Algorithms/2. Binary Search/code.c
+++ Searching Algorithms/2. Binary Search/code.c
@@ -1,4 +1,42 @@
 #include <stdio.h>
+
+int binarySearch(int array[], int targetNumber, int low, int high);
+
 int main(void)
 {
+    int arr[] = {1, 7, 3, 6, 1};
+    int targetNumber = 3;
+    int arrLength = sizeof(arr) / sizeof(arr[0]);
+    int indexOfNumber = binarySearch(arr, targetNumber, 0, arrLength - 1);
+    if (indexOfNumber == -1)
+    {
+        printf("Number Not Found");
+    }
+    else
+    {
+
+        printf("Index of target number: %i", indexOfNumber);
+    }
+}
+
+int binarySearch(int array[], int targetNumber, int low, int high)
+{
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (array[mid] == targetNumber)
+        {
+            return mid;
+        }
+        else if (array[mid] < targetNumber)
+        {
+            low = mid++;
+        }
+        else
+        {
+            high = mid--;
+        }
+    }
+    return -1;
 }
